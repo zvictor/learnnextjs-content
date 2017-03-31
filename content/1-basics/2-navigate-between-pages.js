58084e4ec8520d7f61e4bc3b15f1174ec34b6fb8
Ole Goode <olegoode@gmail.com>
2017-03-31 00:46:39 -0400

    Missed a typo

--- content/1-basics/2-navigate-between-pages.js
+++ content/1-basics/2-navigate-between-pages.js
@@ -165,7 +165,7 @@ What has happened with the above code change?
     text: `
 ## Link is Just a Higher Order Component (HOC)
 
-Actually, the style prop on \`next/link\` has no effect. That's because \`next/link\` is just a [higher order component](https://facebook.github.io/react/docs/higher-order-components.html) which only accepts the "href" and some similar props. If you need to style it, you need to do it to the underline component.
+Actually, the style prop on \`next/link\` has no effect. That's because \`next/link\` is just a [higher order component](https://facebook.github.io/react/docs/higher-order-components.html) which only accepts the "href" and some similar props. If you need to style it, you need to do it to the underlying component.
 
 In this case, it's our anchor.
     `
