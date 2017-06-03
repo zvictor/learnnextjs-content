431ce8f5f5d07221b236d57c14b933ff94f049b5
Guil Sa <guilsa001@gmail.com>
2017-06-03 14:44:19 -0700

    Clarify git clone not necessary if you're coming from previous lesson

--- content/1-basics/2-navigate-between-pages.js
+++ content/1-basics/2-navigate-between-pages.js
@@ -28,7 +28,7 @@ Let's see how to use it.
       text: `
 ## Setup
 
-In order to follow along with this lesson, you need to have a simple Next.js app. For that, download the following example app:
+In order to follow along with this lesson, you need to have a simple Next.js app. For that, continue your work from the previous lesson or download the following example app:
 
 ~~~bash
 git clone https://github.com/arunoda/learnnextjs-demo.git
