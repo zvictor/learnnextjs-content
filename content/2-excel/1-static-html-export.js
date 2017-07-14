a590bc0b6144fc2e260aa587388cbf4e32e00fbf
Thiago Guimarães <thiagogsr@gmail.com>
2017-07-14 01:38:21 -0300

    Add missing space after bracket

--- content/2-excel/1-static-html-export.js
+++ content/2-excel/1-static-html-export.js
@@ -167,7 +167,7 @@ module.exports = {
     return {
       '/': { page: '/' },
       '/about': { page: '/about' },
-      '/p/hello-nextjs': {page: '/post', query: { title: "Hello Next.js" } },
+      '/p/hello-nextjs': { page: '/post', query: { title: "Hello Next.js" } },
       '/p/learn-nextjs': { page: '/post', query: { title: "Learn Next.js is awesome" } },
       '/p/deploy-nextjs': { page: '/post', query: { title: "Deploy apps with Zeit" } }
     }
